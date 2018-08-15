0c797294aa2245bf63f07fe406d474da6fef4580
Md. Masud <jamdmasud@gmail.com>
2018-08-15 06:43:53 +0600

    Initial commit

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc; 
+using System.Linq;
+using TodoBLL.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using TodoDAL.Entities;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        [HttpGet, Authorize]
+        public IActionResult GetAll()
+        {
+            return Ok(_todoRepository.GetAll());
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(int id)
+        {
+            var item = _todoRepository.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            
+            return new NoContentResult();
+        }
+    }
+
+}
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,43 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger; 
+using TodoBLL.Abstract;
+using TodoBLL.Concrete;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TodoDAL;
+using UnitOfWorkRepository;
 
 namespace TodoApi
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=DESKTOP-0UTFTAA\MASUD;Database=EFCoreStudentDb;Trusted_Connection=True;";
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection)); 
+
+            //Add dependency injection
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "http://localhost:45049",
+                        ValidAudience = "http://localhost:45049",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("IxrAjDoa2FqElO7IhrSrUJELhUckePEPVpaePlS_Xaw"))
+                    };
+                });
+
             // Add framework services.
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseAuthentication();
             app.UseMvc();
         }
+
     }
 }
